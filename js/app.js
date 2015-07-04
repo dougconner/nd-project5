a184365e142a8c3327c8e2501b30a78f4838d4fe
Doug Conner <dconner@connerbase.com>
2015-07-04 11:28:56 -0700

    feat: Add Places lookup to get coordinates. Basics working, need to add lookup!

--- js/app.js
+++ js/app.js
@@ -1,130 +1,104 @@
-// function initialize() {
-//     var mapCanvas = document.getElementById('map-canvas');
-//     var mapOptions = {
-//         center: new google.maps.LatLng(44.5403, -78.5463),
-//         zoom: 8,
-//         mapTypeId: google.maps.MapTypeId.ROADMAP
-//     };
-//     var map = new google.maps.Map(mapCanvas, mapOptions);
-// }
-// google.maps.event.addDomListener(window, 'load', initialize);
-var wineries = ko.observableArray([
+var wineries = [
     {"name": "Epoch Estate Wines",
     "lat": 35.5437,
     "lng": -120.8274
     },
     {"name": "Sculpterra Winery",
-    "lng": 35.6064,
-    "lng": -120.6073 },
+    "lat": 35.6064,
+    "lng": -120.6073
+    },
     {"name": "Paso Robles",
-    "lng": 35.64222479297878,
-    "lat": -120.68633100585936}
-    ]);
-
-
-$(document).ready(function () {
-   ko.applyBindings(viewModel);
-});
-
-function MyViewModel() {
-    var self = this;
-    self.mapOne = ko.observable({
-        // lat: ko.observable(35.6012),
-        // lng:ko.observable(-120.6108)
-        lat: ko.observable(35.64222479297878),
-        lng:ko.observable(-120.68633100585936)
-        // lat: ko.observable(35.5437),
-        // lng:ko.observable(-120.8274)
-    });
-
-
-}
-
-// derived from http://jsfiddle.net/schmidlop/5eTRV/10/
-ko.bindingHandlers.map = {
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        var mapObj = ko.utils.unwrapObservable(valueAccessor());
-        var latLng = new google.maps.LatLng(
-            ko.utils.unwrapObservable(mapObj.lat),
-            ko.utils.unwrapObservable(mapObj.lng));
-        var mapOptions = { center: latLng,
-                          zoom: 11,
-                          // styles: styleArray,
-                          mapTypeId: google.maps.MapTypeId.ROADMAP};
-
-        // mapObj.googleMap = new google.maps.Map(element, mapOptions);
-        mapObj.googleMap = new google.maps.Map(element, mapOptions);
-
-        console.log("wineries[i].lat: ", wineries()[0].lat);
-        // setMarkers(map, wineries);
-        for (var i = 0; i < wineries().length; i++) {
-            var markerLatLng = new google.maps.LatLng(
-                wineries()[i].lat,
-                wineries()[i].lng);
-            var mapObj.marker = new google.maps.Marker({
-                map: mapObj.googleMap,
-                position: markerLatLng,
-                title: "here",
-                // title: wineries()[i].name,
-                draggable: true
-            });
-        }
-        // mapObj.marker = new google.maps.Marker({
-        //     map: mapObj.googleMap,
-        //     position: latLng,
-        //     title: "You Are Here",
-        //     draggable: true
-        // });
-
-        mapObj.onChangedCoord = function(newValue) {
-            var latLng = new google.maps.LatLng(
-                ko.utils.unwrapObservable(mapObj.lat),
-                ko.utils.unwrapObservable(mapObj.lng));
-            mapObj.googleMap.setCenter(latLng);
-        };
-
-        mapObj.onMarkerMoved = function(dragEnd) {
-            var latLng = mapObj.marker.getPosition();
-            mapObj.lat(latLng.lat());
-            mapObj.lng(latLng.lng());
-        };
-
-        mapObj.lat.subscribe(mapObj.onChangedCoord);
-        mapObj.lng.subscribe(mapObj.onChangedCoord);
-
-        google.maps.event.addListener(mapObj.marker, 'dragend', mapObj.onMarkerMoved);
-
-        $("#" + element.getAttribute("id")).data("mapObj",mapObj);
+    "lat": 35.64222479297878,
+    "lng": -120.68633100585936
     }
-};
-
-var styleArray = [
-  {
-    featureType: "all",
-    stylers: [
-      { saturation: -80 }
-    ]
-  },{
-    featureType: "road.arterial",
-    elementType: "geometry",
-    stylers: [
-      { hue: "#00ffee" },
-      { saturation: 50 }
-    ]
-  },{
-    featureType: "poi.attraction",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "poi.business",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  }
 ];
 
+var initialData = [
+    { name: "me",
+    url: "www.solarheatengines.com",
+    addr1: "12170 Cenegal Rd",
+    addr2: "Atas, CA",
+    zip: 93422,
+    lat: "",
+    lng: ""
+    }
+];
 
-var viewModel = new MyViewModel();
+// Reference for Places table: http://jsfiddle.net/rniemeyer/gZC5k/
+
+function MyViewModel(places) {
+    // 'use strict';
+    var self = this;
+    self.placeFinderView = true;
+    self.places = ko.observableArray(ko.utils.arrayMap(places, function(place) {
+        return { name: place.name,
+            url: place.url,
+            addr1: place.addr1,
+            addr2: place.addr2,
+            zip: place.zip,
+            lat: place.lat,
+            lng: place.lng
+        };
+    }));
+
+    self.addPlace = function() {
+        self.places.push({
+            name: "",
+            url: "",
+            addr1: "",
+            addr2: "",
+            zip: "",
+            lat: "",
+            lng: ""
+        });
+    };
+
+    self.removePlace = function(place) {
+        self.places.remove(place);
+    };
+
+    self.lookupPlace = function(place) {
+        console.log("lookupPlace code here");
+    };
+
+    self.save = function() {
+        self.lastSavedJson(JSON.stringify(ko.toJS(self.places), null, 2));
+    };
+
+    self.lastSavedJson = ko.observable("");
+}
+
+function setMarkers(map, locations) {
+    // add markers to map
+    for (var i = 0; i < locations.length; i++) {
+        var location = locations[i];
+        var latLng = new google.maps.LatLng(location.lat, location.lng);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: location.name
+        });
+    }
+}
+
+
+function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+        center: new google.maps.LatLng(35.64222479297878, -120.68633100585936),
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    return map;
+}
+
+$(document).ready(function () {
+   var map = initialize();
+   setMarkers(map, wineries);
+});
+
+// this.placefind;
+
+ko.applyBindings(new MyViewModel(initialData));
+// var viewModel = new MyViewModel(initialData);
